8625793c568b72e2c07fc68225b8a9f5b21d655f
JOBLOLI <l_henryz@hotmail.com>
2026-03-24 18:18:41 -0400

    cart items added to checkout page

--- frontend/src/app/pages/checkout-page/checkout-page.ts
+++ frontend/src/app/pages/checkout-page/checkout-page.ts
@@ -2,6 +2,7 @@ import { Component, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Product } from '../../models/product';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout-page',
@@ -11,26 +12,13 @@ import { Product } from '../../models/product';
   styleUrl: './checkout-page.css',
 })
 export class CheckoutPage {
-  cartItems = signal<Product[]>([
-    {
-      id: 1,
-      name: 'Laptop',
-      description: '',
-      price: 2499.99,
-      rating: 4.5,
-      imageUrl: 'https://picsum.photos/seed/laptop/400/250',
-      categories: [],
-    },
-    {
-      id: 2,
-      name: 'Keyboard',
-      description: '',
-      price: 129.99,
-      rating: 4,
-      imageUrl: 'https://picsum.photos/seed/keyboard/400/250',
-      categories: [],
-    },
-  ]);
+  constructor(public cartService: CartService) {}
 
-  total = computed(() => this.cartItems().reduce((sum, item) => sum + item.price, 0));
+  cartItems() {
+    return this.cartService.getItems()();
+  }
+
+  total() {
+    return this.cartService.total();
+  }
 }
